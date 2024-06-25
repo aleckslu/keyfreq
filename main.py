69d5196e96b4cb742f8a891e5a962b127e755cc8
aleckslu <50436165+aleckslu@users.noreply.github.com>
2024-06-25 04:07:26 -0700

    properly log for control characters

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from pynput.keyboard import Key, Listener
 import ctypes
 import psutil
 
+# TODO: more precise / convenient save directory
+### Settings
+# Filenames for main key log and backup file
 JSON_FILE_NAME = "key-freq.json"
 BACKUP_FILE_NAME = "key-freq-backup.json"
+
+# Save every x number of keystrokes
 SAVE_INTERVAL = 500
 
-KEY_LOG = {}
+# print each keypress onto terminal
+LOG_TO_TERMINAL = True
 
 LOCKED_IN_GARBAGE_COLLECTION_LIMIT = 5
+
+### Global Variables
+KEY_LOG = {}
+
 
 MODIFIER_KEYS = [
     Key.alt,
@@ -39,11 +49,43 @@ REMAP = {
     Key.shift_l: Key.shift,
 }
 
+# TODO control keys for more symbols
+CONTROL_KEYS = {
+    "'\\x01'": "a",
+    "'\\x02'": "b",
+    "'\\x03'": "c",
+    "'\\x04'": "d",
+    "'\\x05'": "e",
+    "'\\x06'": "f",
+    "'\\x07'": "g",
+    "'\\x08'": "h",
+    "'\\x09'": "i",
+    "'\\x0a'": "j",
+    "'\\x0b'": "k",
+    "'\\x0c'": "l",
+    "'\\x0d'": "m",
+    "'\\x0e'": "n",
+    "'\\x0f'": "o",
+    "'\\x10'": "p",
+    "'\\x11'": "q",
+    "'\\x12'": "r",
+    "'\\x13'": "s",
+    "'\\x14'": "t",
+    "'\\x15'": "u",
+    "'\\x16'": "v",
+    "'\\x17'": "w",
+    "'\\x18'": "x",
+    "'\\x19'": "y",
+    "'\\x1a'": "z",
+    "'\\x1b'": "[",
+    "'\\x1c'": "\\",
+    "'\\x1d'": "]",
+    "'\\x1e'": "^",
+    "'\\x1f'": "_",
+}
+
 keys_currently_down = []
 total_keypresses = 0
-
-
-###
 
 
 def load_and_backup_file():
@@ -56,25 +98,24 @@ def load_and_backup_file():
 
         with open(BACKUP_FILE_NAME, "w") as b:
             json.dump(KEY_LOG, b, indent=2)
-            print(f"Backup Made to {BACKUP_FILE_NAME}")
+            print(f"Back up made to {BACKUP_FILE_NAME}")
 
     except Exception as e:
-        print("Error loading or backingup data: ", e)
+        print("Error loading or backing up data: ", e)
         print("Data will still be saved to a file")
+
 
 def set_total():
     global KEY_LOG
 
     attribute = "_TOTAL_LOG"
     KEY_LOG.setdefault(attribute, {})
-    
-    print(KEY_LOG[attribute])
+
     for process_name, log in KEY_LOG.items():
         if not process_name == attribute:
             for key, count in log.items():
                 KEY_LOG[attribute].setdefault(key, 0)
                 KEY_LOG[attribute][key] += count
-    print(KEY_LOG[attribute])
 
 
 def get_process_name():
@@ -91,7 +132,7 @@ def get_process_name():
         print(f"Error getting process name: {e}")
 
 
-def record_key(key, process_name):
+def record_key(process_name, key):
     global KEY_LOG, total_keypresses
 
     KEY_LOG.setdefault(process_name, {})
@@ -122,21 +163,28 @@ def is_key_a_symbol(key):
 
 def key_to_str(key):
     s = str(key)
+
     if not is_key_a_symbol(key):
-        s = f"<{s[4:]}>"
+        # print(f".keys for key: {key} dir: {key.__dict__.keys()}")
+        # print(f".values for key: {key} dir: {key.__dict__.values()}")
+        s = f"<{key._name_}>"
+    elif s in CONTROL_KEYS:
+        s = CONTROL_KEYS[s]
+        print(f"ctrl changed key: {s}")
     else:
-        s = s.encode("latin-1", "backslashreplace").decode("unicode-escape")
-        s = s[1:-1]  # trim the leading and trailing quotes
+        s = key.char
+        # s = s.encode("latin-1", "backslashreplace").decode("unicode-escape")
+        # s = s[1:-1]  # trim the leading and trailing quotes
     return s
 
 
 def key_down(key):
     if key in keys_currently_down:
         return
-    keys_currently_down.append(key)
-    record_key(key_to_str(key), get_process_name())
 
-    # TODO: remove - Temporary workaround for KeyboardInterrupt not working
+    keys_currently_down.append(key)
+    record_key(get_process_name(), key_to_str(key))
+
     if key == Key.f17:
         print("f17, exiting")
         set_total()
@@ -160,6 +208,7 @@ def key_up(key):
 
 def preprocess_key(key, f):
     k = key
+
     if key in REMAP:
         k = REMAP[key]
 
@@ -171,8 +220,6 @@ def preprocess_key(key, f):
 
 def main():
     load_and_backup_file()
-
-
     print("Key Freqwincy will begin recording keypresses now")
     with Listener(
         on_press=(lambda key: preprocess_key(key, key_down)),
